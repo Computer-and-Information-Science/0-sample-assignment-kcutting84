cd2b45c77130682ab2ecb915738a8f175e8198f8
kcutting84 <krbishop1984@gmail.com>
2025-08-28 15:42:43 +0000

    completed assignment

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,17 @@
-// YOUR NAME HERE
+// Krystlelee Cutting
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+
+   int first = 15;
+   int second = 22;
+   int sum;
+
+   sum = first + second;
+
+  cout << "The Sum is " << sum;
+  return 0;
+
 
 }
